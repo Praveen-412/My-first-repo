58eb3d3eec860742c1e3fcb335f174f70afa1bb0
Praveen Kumar <pk6865421@gmail.com>
2025-11-25 11:50:23 +0530

    Second commit

---
+++ swap.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int A,B,temp;
+printf("Enter the values of A and B");
+scanf("%d%d",&A,&B);
+A=temp;
+A=B;
+temp=A;
+printf("The value is a=%d and b=%d after swapping",A,B);
+return 0;
+}
