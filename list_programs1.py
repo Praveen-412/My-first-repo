daae8cf24ed4e1c244f5d982d51d6ec01f7766d6
Praveen Kumar <pk6865421@gmail.com>
2025-12-02 12:10:29 +0530

    file added

---
+++ list_programs1.py
@@ -0,0 +1,21 @@
+import os
+
+def list_terminal_programs():
+    paths = os.environ.get("PATH", "").split(":")
+    programs = set()
+
+    for path in paths:
+        if os.path.isdir(path):
+            for item in os.listdir(path):
+                full_path = os.path.join(path, item)
+                if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+                    programs.add(item)
+
+   
+   
+    for prog in sorted(programs):
+        print(prog)
+
+if __name__ == "__main__":
+    list_terminal_programs()
+
